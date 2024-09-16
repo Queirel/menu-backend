76369c966c45105e21bb85936fd5b690869af74e
Fede <federicoqueirel@hotmail.com>
2024-09-16 00:48:36 -0300

    added users, restaurants, menus

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { RestaurantsService } from './restaurants/restaurants.service';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { MenusController } from './menus/menus.controller';
+import { MenusService } from './menus/menus.service';
+import { MenusModule } from './menus/menus.module';
+import { UsersController } from './users/users.controller';
+import { RestaurantsController } from './restaurants/restaurants.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'menu',
+      entities: [],
+      synchronize: true,
+      logging: false,
+    }),
+    UsersModule,
+    RestaurantsModule,
+    MenusModule,
+  ],
+  controllers: [MenusController, UsersController, RestaurantsController],
+  providers: [RestaurantsService, MenusService, UsersService],
 })
 export class AppModule {}
